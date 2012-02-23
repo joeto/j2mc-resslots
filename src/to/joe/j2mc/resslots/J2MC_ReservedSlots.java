18a4fc46342e307e4c1434afa0b7d9e9504ee91f
Ammar Askar <ammar_askar@hotmail.com>
2012-02-23 23:32:39 +0500

    Bye soft limit. You will NOT be missed :D

--- src/to/joe/j2mc/resslots/J2MC_ReservedSlots.java
+++ src/to/joe/j2mc/resslots/J2MC_ReservedSlots.java
@@ -1,17 +1,18 @@
 package to.joe.j2mc.resslots;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerPreLoginEvent;
-import org.bukkit.event.player.PlayerPreLoginEvent.Result;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerLoginEvent.Result;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import to.joe.j2mc.core.J2MC_Manager;
 
 public class J2MC_ReservedSlots extends JavaPlugin implements Listener {
 
-    private int softLimit;
+    private int howmanybeforekicking;
 
     @Override
     public void onDisable() {
@@ -21,18 +22,35 @@ public class J2MC_ReservedSlots extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
-        this.softLimit = this.getConfig().getInt("softlimit", 30);
+        this.howmanybeforekicking = this.getConfig().getInt("howmanybeforekicking", 10);
         this.getLogger().info("Reserved slots module enabled");
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onPlayerPreLogin(PlayerPreLoginEvent event) {
-        if (this.getServer().getOnlinePlayers().length >= this.softLimit) {
-            final boolean isAdmin = J2MC_Manager.getPermissions().isAdmin(event.getName());
-            final boolean isDonator = J2MC_Manager.getPermissions().hasFlag(event.getName(), 'd');
-            if (!isAdmin && !isDonator) {
-                event.disallow(Result.KICK_OTHER, "Server full! For a reserved slot see donate.joe.to");
-            }
-        }
-    }
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onPlayerPreLogin(PlayerLoginEvent event) {
+		boolean isAdmin = J2MC_Manager.getPermissions().isAdmin(event.getPlayer().getName());
+		boolean isDonator = J2MC_Manager.getPermissions().hasFlag(event.getPlayer().getName(), 'd');
+		if(this.getServer().getOnlinePlayers().length >= this.getServer().getMaxPlayers()){
+			if(!isAdmin && !isDonator){
+				event.disallow(Result.KICK_FULL, "Server full! For a reserved slot see donate.joe.to");
+			}else{
+				this.getLogger().info("Donator/Admin "  + event.getPlayer().getName() + " attempting to join server while full");
+				if((this.getServer().getMaxPlayers() + this.howmanybeforekicking) <= this.getServer().getOnlinePlayers().length){
+					this.getLogger().info("We are above the player limit + " + howmanybeforekicking + " cap, kicking someone to make room");
+					for(Player plr : this.getServer().getOnlinePlayers()){
+						if(!J2MC_Manager.getPermissions().isAdmin(plr.getName()) || !J2MC_Manager.getPermissions().hasFlag(plr.getName(), 'd')){
+							plr.kickPlayer("Player with reserved slot joined, see donate.joe.to");
+							break;
+						}
+					}
+					event.allow();
+					this.getLogger().info("Allowed player " + event.getPlayer().getName() + " through");
+				}else{
+					event.setResult(Result.ALLOWED);
+					event.allow();
+					this.getLogger().info("Allowed player " + event.getPlayer().getName() + " through");
+				}
+			}
+		}
+	}
 }
