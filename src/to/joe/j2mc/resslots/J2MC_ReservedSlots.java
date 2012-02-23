b422bf0123fa7b12f919a254f103e1a28c687512
Matt Baxter <matt@phozop.net>
2012-02-23 18:37:04 -0500

    Cleanup

--- src/to/joe/j2mc/resslots/J2MC_ReservedSlots.java
+++ src/to/joe/j2mc/resslots/J2MC_ReservedSlots.java
@@ -1,6 +1,6 @@
 package to.joe.j2mc.resslots;
 
-import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -23,18 +23,18 @@ public class J2MC_ReservedSlots extends JavaPlugin implements Listener {
         this.getLogger().info("Reserved slots module enabled");
     }
 
-	@EventHandler(priority = EventPriority.LOWEST)
-	public void onPlayerPreLogin(PlayerLoginEvent event) {
-		boolean isAdmin = J2MC_Manager.getPermissions().isAdmin(event.getPlayer().getName());
-		boolean isDonator = J2MC_Manager.getPermissions().hasFlag(event.getPlayer().getName(), 'd');
-		if(this.getServer().getOnlinePlayers().length >= this.getServer().getMaxPlayers()){
-			if(!isAdmin && !isDonator){
-				event.disallow(Result.KICK_FULL, "Server full! For a reserved slot see donate.joe.to");
-			}else{
-			    event.setResult(Result.ALLOWED);
-				event.allow();
-				this.getLogger().info("Allowed player " + event.getPlayer().getName() + " through");
-			}
-		}
-	}
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerPreLogin(PlayerLoginEvent event) {
+        if (this.getServer().getOnlinePlayers().length >= this.getServer().getMaxPlayers()) {
+            final boolean isAdmin = J2MC_Manager.getPermissions().isAdmin(event.getPlayer().getName());
+            final boolean isDonator = J2MC_Manager.getPermissions().hasFlag(event.getPlayer().getName(), 'd');
+            if (!isAdmin && !isDonator) {
+                event.disallow(Result.KICK_FULL, ChatColor.RED + "Server full!" + ChatColor.WHITE + " For a reserved slot see donate.joe.to");
+            } else {
+                event.setResult(Result.ALLOWED);
+                event.allow();
+                this.getLogger().info("Allowed player " + event.getPlayer().getName() + " through");
+            }
+        }
+    }
 }
