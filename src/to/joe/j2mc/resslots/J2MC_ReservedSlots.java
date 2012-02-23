e0923c06f2e042b4dab84431cf96e484cb6b0895
mbaxter <github@phozop.net>
2012-02-23 13:54:08 -0500

    All better

--- src/to/joe/j2mc/resslots/J2MC_ReservedSlots.java
+++ src/to/joe/j2mc/resslots/J2MC_ReservedSlots.java
@@ -12,8 +12,6 @@ import to.joe.j2mc.core.J2MC_Manager;
 
 public class J2MC_ReservedSlots extends JavaPlugin implements Listener {
 
-    private int howmanybeforekicking;
-
     @Override
     public void onDisable() {
         this.getLogger().info("Reserved slots module disabled");
@@ -22,7 +20,6 @@ public class J2MC_ReservedSlots extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
-        this.howmanybeforekicking = this.getConfig().getInt("howmanybeforekicking", 10);
         this.getLogger().info("Reserved slots module enabled");
     }
 
@@ -34,22 +31,9 @@ public class J2MC_ReservedSlots extends JavaPlugin implements Listener {
 			if(!isAdmin && !isDonator){
 				event.disallow(Result.KICK_FULL, "Server full! For a reserved slot see donate.joe.to");
 			}else{
-				this.getLogger().info("Donator/Admin "  + event.getPlayer().getName() + " attempting to join server while full");
-				if((this.getServer().getMaxPlayers() + this.howmanybeforekicking) <= this.getServer().getOnlinePlayers().length){
-					this.getLogger().info("We are above the player limit + " + howmanybeforekicking + " cap, kicking someone to make room");
-					for(Player plr : this.getServer().getOnlinePlayers()){
-						if(!J2MC_Manager.getPermissions().isAdmin(plr.getName()) || !J2MC_Manager.getPermissions().hasFlag(plr.getName(), 'd')){
-							plr.kickPlayer("Player with reserved slot joined, see donate.joe.to");
-							break;
-						}
-					}
-					event.allow();
-					this.getLogger().info("Allowed player " + event.getPlayer().getName() + " through");
-				}else{
-					event.setResult(Result.ALLOWED);
-					event.allow();
-					this.getLogger().info("Allowed player " + event.getPlayer().getName() + " through");
-				}
+			    event.setResult(Result.ALLOWED);
+				event.allow();
+				this.getLogger().info("Allowed player " + event.getPlayer().getName() + " through");
 			}
 		}
 	}
